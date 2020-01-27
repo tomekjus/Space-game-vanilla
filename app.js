29974dc7737d983c233cf8b037918378ccc7cfc1
Tomasz <t.juskiewicz@betterandmore.eu>
2020-01-27 17:24:07 +0100

    add new planets

--- app.js
+++ app.js
@@ -5,30 +5,48 @@ const btnItems = document.querySelectorAll('.btnall');
 const up = document.querySelectorAll('.up');
 const levels = Array.from(document.querySelectorAll('.level'));
 const passive = document.getElementById('passive');
-const upText = document.querySelectorAll('.upText');
 
 
 let gold = 0;
+let passiveGold = [0];
 let planets = [{
         name: "mars",
         level: 0,
-        levelCost: [20, 20, 30, 40],
-        passiveGold: [1, 3, 6, 9]
+        passiveGold: [1, 3, 6, 9],
+        levelCost: [10, 30, 60, 90],
+        buildings: [
+            {
+                requiredLevel: 1,
+                building: "water"
+            },
+            {
+                requiredLevel: 2,
+                building: "tree"
+            },
+            {
+                requiredLevel: 3,
+                building: "mountains"
+            },
+            {
+                requiredLevel: 4,
+                building: "lava"
+            }
+        ]
     },
     {
         name: "neptune",
         level: 0,
         passiveGold: [3, 6, 9, 12],
-        levelCost: [10, 20, 30, 40]
+        levelCost: [50, 100, 150, 200]
     },
     {
         name: "venus",
         level: 0,
         passiveGold: [10, 20, 30, 40],
-        levelCost: [20, 40, 30, 40]
+        levelCost: [200, 300, 400, 500]
     }
 ];
-let passiveGlobal = gold + planets.passiveGold;
+
 
 
 function startHandler() {
@@ -44,28 +62,43 @@ function exitHandler() {
 
 function addGold() {
     gold++;
-    pocket.textContent = `${gold}g`;
+    pocket.textContent = `Gold: ${gold}`;
 }
 
 function upgrade(planet) {
     if (gold >= planets[planet].levelCost[planets[planet].level]) {
         gold -= planets[planet].levelCost[planets[planet].level];
-        pocket.textContent = `${gold}g`
+        pocket.textContent = `Gold: ${gold}`
         planets[planet].level += 1;
         levels[planet].textContent = `${planets[planet].level} lvl`;
-        upText.textContent = `${planets[planet].levelCost[planets[planet]]}g`;
+        up[planet].textContent = `Upgrade - cost:${planets[planet].levelCost[planets[planet].level]}g`;
+        passiveGold.push(planets[planet].passiveGold[planets[planet].level]);
 
+        if(planets[planet].level == 1) {
+             const planetName = planets[planet].name;
+             const planetElement = document.getElementById(planetName);
+             planetElement.style.display = "block";
+        }
 
-        // setInterval(function() {
-        //     gold = gold + planets[planet].passiveGold[planets[planet].level - 1];
-        //     pocket.textContent = `${gold}g`;
-        //     passive.textContent = `${planets[planet].passiveGold[planets[planet].level - 1]}`
-        // }, 1000);
-
-
+        build();
     } else {
-        alert("Za mało złota kurwiu");
+        alert("Za mało złota mój kurwo demonie");
     }
+}
+
+function build() {
+    console.log("test");
+}
+
+function passiveGlobal() {
+    let sum = 0;
+    passiveGold.forEach(item => {
+        sum += item;
+    });
+
+    gold = gold + sum;
+    pocket.textContent = `Gold: ${gold}`;
+    passive.textContent = `Passive Gold: ${sum}/s`;
 }
 
 setInterval(passiveGlobal, 1000);
